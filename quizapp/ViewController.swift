dfa4e6492017b10caaf19e64100e92b765fe1258
Harsh <harsh20221@gmail.com>
2024-07-20 04:19:45 +0530

    Fixed Buttons conections

--- quizapp/ViewController.swift
+++ quizapp/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+let quest=["Four+Two Is Equal to Six ",
+    " Five-3 is equal to 2 ",
+   "Three + 8 < 10"]
+    var currentquestion=0;
+
+
+    @IBAction func Button(_ sender: UIButton) {
+        currentquestion+=1;
+        Questiontextcontroller.text=quest[currentquestion];
+    }
+    @IBOutlet var TrueButton: UIButton!
+    @IBOutlet var FalseButton: UIButton!
+    @IBOutlet var Questiontextcontroller: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Questiontextcontroller.text=quest[currentquestion];
+        
+        
+        
+        
+        
+        
     }
 
+    
+
+    
 
 }
 
